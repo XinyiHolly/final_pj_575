60444da7a68cac28d6d5e7e7a6b7174ccb03763a
aeibergen <Student@shc.wisc.edu>
2017-05-08 17:27:03 -0500

    Multiple Updates
    Involved responsive design for modals, stylings for buttons and
    panel-background, and fix for map scale.

--- js/main.js
+++ js/main.js
@@ -17,19 +17,19 @@ function setMap(){
 	//var margin = {top: 10, left: 10, bottom: 10, right: 10}
 	var width = $("#mapDiv").innerWidth()
 	  , width = width //- margin.left - margin.right
-	  , mapRatio = 1
+	  , mapRatio = 1/1.5
 	  , height = width * mapRatio;
 
 	function resize() {
     // adjust things when the window size changes
-    width = $("#mapDiv").innerWidth()
+    width = $("#mapDiv").innerWidth();
     width = width //- margin.left - margin.right;
     height = width * mapRatio;
 
     // update projection
     projection
-        .translate([width / 2, height / 2])
-        .scale(width);
+        .translate([width/2 , height/2])
+        .scale(width+300);
 
     // resize the map container
     map
@@ -311,9 +311,13 @@ function updateAirportDelays(airports,delayType){
 			//Add airport events for click and highlight
 			.on("click", function (d) {
 				callRoutes(d.origincode);
+				updatePanel(airports)
 			})
 			.on("mouseover", function(d){
 				highlightAirport(d);
+			})
+			.on("mouseenter", function(){
+				$(this).css("cursor","pointer");
 			})
 			.on("mouseout", function(d){
 				dehighlightAirport(d.origincode)
@@ -324,6 +328,14 @@ function updateAirportDelays(airports,delayType){
       airports.append("desc")
           .text('{"fill":"blue", "stroke-width": "0.5px", "stroke-opacity": "0.65"}');
 }
+
+//Update the panel with airport delay information
+function updatePanel(airports){
+    var content = "<div id='panelTitle'><h2><img src='images/anchor.png'>Port of "+feature.properties.port+"<img src='images/"+feature.properties.country+".svg'></h2></div>";
+    content += "<div id='panelPic'><img src='"+airports.properties.img+"' align='middle'></div>";
+    content += "<div id='panelDesc'><p>"+airports.properties.desc+"</p></div>";
+    $("#update-panel").html(content);
+};
 
 //Returns the radius given predefined classes
 function scaleAirportDelay(val){
@@ -355,7 +367,7 @@ function highlightAirport(prop){
 
     //change stroke
     var selected = d3.selectAll(".airports_" + prop.origincode)
-        .style("fill", "#000080")
+        .style("fill", "#ccac00")
         .moveToFront();
 
     //call set label
@@ -752,48 +764,40 @@ d3.select(".container2")
 		$(window).on("resize",function(){
 			if ($(window).width()<992){
 				$("#side-panel").appendTo("#bottom");
-				console.log("HERE")
+				//console.log("HERE")
 			} else{
 				$("#side-panel").prependTo("#bottom");
 			}
 		})
 	})
 
-	//display loader for the start page
+	//display overview window and loader for the start page
 	$(window).on("load",function(){
-		$("#myModal1").modal("show")
-	})
-	
-	$("#myModal1").on("click",function(){
+		$("#myModal1").modal("show");
 		$(".loader").show();
-		/*
-		$(window).on("load",function(){
-			$(".loader").fadeOut();
-		})
-		*/
 	})
 
+	//create start page loader
+	d3.select("body")
+		.append("div")
+		.attr("class","loader")
+		.style("display","none")
+	/*
+	$(window).on("load",function(){
+		setTimeout(removeLoader,5000)
+	});
+	function removeLoader(){
+		$(".loader").fadeOut(3800,function(){
+			$(".loader").remove();
+		});
+	}
+	*/
+	
 //tutorial button interaction
 $(".tutorial-Button").on("click",function(){
 	$(".OverviewBox").fadeOut(350);
 	$(".TutorialBox").fadeIn(350);
 })
-
-//create start page loader
-d3.select("body")
-	.append("div")
-	.attr("class","loader")
-	.style("display","none")
-/*
-$(window).on("load",function(){
-	setTimeout(removeLoader,5000)
-});
-function removeLoader(){
-	$(".loader").fadeOut(3800,function(){
-		$(".loader").remove();
-	});
-}
-*/
 
 //display intro window and grayout background again when 'About' is clicked
 $(".menu-button1").on("click",function(){
@@ -862,4 +866,5 @@ $("#return_default").on("click",function(){
 	var checkBoxes = $("input[name=airline]");
 	checkBoxes.prop("checked",true);
 })
+
 })();
